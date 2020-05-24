0c9edb614d7e1741469cd7143565049aa159b669
Vycheslav Kulik <slava.kulik27@gmail.com>
2020-05-24 21:13:30 +0300

    file collector added in class Files

---
+++ src/kpi/iasa/Files.java
@@ -0,0 +1,26 @@
+package kpi.iasa;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class Files {
+
+    ArrayList<File> files = new ArrayList<>();
+    ArrayList<File> folders = new ArrayList<>();
+
+    public ArrayList<File> prepareFiles(){
+        
+        folders.add(new File("C:\\Study\\3course_2sem\\Parallel\\course\\files\\test_neg"));
+        folders.add(new File("C:\\Study\\3course_2sem\\Parallel\\course\\files\\test_pos"));
+        folders.add(new File("C:\\Study\\3course_2sem\\Parallel\\course\\files\\train_neg"));
+        folders.add(new File("C:\\Study\\3course_2sem\\Parallel\\course\\files\\train_pos"));
+        folders.add(new File("C:\\Study\\3course_2sem\\Parallel\\course\\files\\train_unsup"));
+
+        for (File folder : folders){
+            for (File file : folder.listFiles()){
+                files.add(file.getAbsoluteFile());
+            }
+        }
+        return files;
+    }
+}
