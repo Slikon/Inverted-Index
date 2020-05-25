8d5fea4084dfe979b14cd1e0f0fa3ae370a3deb5
Vycheslav Kulik <slava.kulik27@gmail.com>
2020-05-25 15:03:56 +0300

    added: merge into final result; fixed: time counter (now after merging)

--- src/kpi/iasa/Main.java
+++ src/kpi/iasa/Main.java
@@ -8,13 +8,13 @@ import java.util.HashSet;
 public class Main {
 
     static ArrayList<File> files = new Files().prepareFiles();
-    public static int THREAD_NUM = 100;
+    public static int THREAD_NUM = 4;
+
 
     public static void main(String[] args) throws InterruptedException {
 
-        HashMap<String, HashSet<File>> invertedIndex = new HashMap<>();
-
         InvertedIndex[] threadArray = new InvertedIndex[THREAD_NUM];
+        HashMap<String, HashSet<File>> fullInvertedIndex = new HashMap<>();
 
         long START = System.currentTimeMillis();
         //starting threads
@@ -31,7 +31,23 @@ public class Main {
         for (int numThread = 0; numThread < THREAD_NUM; numThread++){
             threadArray[numThread].join();
         }
+
+        //merging results of all threads into final 'fullInvertedIndex'
+        for (int num = 0; num < THREAD_NUM; num++){
+            threadArray[num].getInvertedIndex().forEach(
+                    (key, value) -> fullInvertedIndex.merge(key, value,
+                            (v1, v2) ->
+                            {
+                                HashSet<File> fileValueV1 = new HashSet<>(v1);
+                                fileValueV1.addAll(v2);
+                                return fileValueV1;
+                            }
+                            ));
+        }
+
         long FINISH = System.currentTimeMillis();
         System.out.println("TIME = " + (FINISH-START));
+
+        System.out.println(fullInvertedIndex.get("hollywood"));
     }
 }
