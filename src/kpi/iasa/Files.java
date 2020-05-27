73fc150801aa1607f1fe0fc49cfaf3db76211154
Vycheslav Kulik <slava.kulik27@gmail.com>
2020-05-27 16:19:17 +0300

    'folders' repaired into 'directories'[5]

--- src/kpi/iasa/Files.java
+++ src/kpi/iasa/Files.java
@@ -6,17 +6,16 @@ import java.util.ArrayList;
 public class Files {
 
     ArrayList<File> files = new ArrayList<>();
-    ArrayList<File> folders = new ArrayList<>();
+
+    File[] directories = {new File("C:\\Study\\3course_2sem\\Parallel\\course\\files\\test_neg"),
+                      new File("C:\\Study\\3course_2sem\\Parallel\\course\\files\\test_pos"),
+                      new File("C:\\Study\\3course_2sem\\Parallel\\course\\files\\train_neg"),
+                      new File("C:\\Study\\3course_2sem\\Parallel\\course\\files\\train_pos"),
+                      new File("C:\\Study\\3course_2sem\\Parallel\\course\\files\\train_unsup")};
 
     public ArrayList<File> prepareFiles(){
 
-        folders.add(new File("C:\\Study\\3course_2sem\\Parallel\\course\\files\\test_neg"));
-        folders.add(new File("C:\\Study\\3course_2sem\\Parallel\\course\\files\\test_pos"));
-        folders.add(new File("C:\\Study\\3course_2sem\\Parallel\\course\\files\\train_neg"));
-        folders.add(new File("C:\\Study\\3course_2sem\\Parallel\\course\\files\\train_pos"));
-        folders.add(new File("C:\\Study\\3course_2sem\\Parallel\\course\\files\\train_unsup"));
-
-        for (File folder : folders){
+        for (File folder : directories){
             for (File file : folder.listFiles()){
                 files.add(file.getAbsoluteFile());
             }
